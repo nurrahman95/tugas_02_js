90c1e30414265b024e14d5e156c8f44233b7ec76
nurrahman <nurrahman0495@gmail.com>
2020-02-26 15:58:17 +0700

    Tugas 02 js
    pengumpulan tugas 02 js

---
+++ tugas2.js
@@ -0,0 +1,15 @@
+var number = 20.0;
+var string = "data string";
+var boolean = false;
+var kosong = "berisi";
+kosong = null;
+var kosong2 ;
+var array = [1,2,3,4,5];
+
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(kosong);
+console.log(kosong2);
+console.log(array[0]);
